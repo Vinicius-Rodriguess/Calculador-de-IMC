e2d0b1cba52156cd711ff4387e1b127be6bbfb64
Tk 000407 <rodrigues.vini.2004@gmail.com>
2023-06-29 16:59:42 -0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
+function handleKeyDown(event) {
+
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    calcular(); 
+};
+
+}
+
 function calcular() {
   const peso = parseFloat(document.querySelector('#peso').value);
   const altura = parseFloat(document.querySelector('#altura').value / 100);
@@ -5,47 +14,70 @@ function calcular() {
   const divResultado = document.querySelector('.resultado');
   const tipoPeso = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
 
-  if (isNaN(peso) || isNaN(altura)){
-    divResultado.innerHTML = 'Favor utilizar apenas Números!';
+   if (peso <=0  || altura <=0) {
+    divResultado.innerHTML = 'Favor verificar os campos!';
     divResultado.style.color = "red";
+    divResultado.classList.add('exibir');
     return;
-  } 
+  } else if(peso >=300  || altura >=3.00) { 
+    divResultado.innerHTML = 'Favor usar numeros realistas!';
+    divResultado.style.color = "red";
+    divResultado.classList.add('exibir');
+    return;
+  } else if (isNaN(peso) || isNaN(altura)) {
+    divResultado.innerHTML = 'Favor fornecer apenas números!';
+    divResultado.style.color = "red";
+    divResultado.classList.add('exibir');
+    return;
+  }
   
   if (resultado < 18.5) {
     divResultado.innerHTML = `IMC ${resultado} - ${tipoPeso[0]}`;
     divResultado.style.color = "#FFA500";
+    divResultado.classList.add('exibir');
   }
   else if (resultado >= 100) {
     divResultado.innerHTML = 'Favor conferir as informaçoes fornecidas!';
     divResultado.style.color = "red";
+    divResultado.classList.add('exibir');
+
   }
   else if (resultado >= 40) {
     divResultado.innerHTML = `IMC ${resultado} - ${tipoPeso[5]}`;
     divResultado.style.color = "#FFA500";
+    divResultado.classList.add('exibir');
+
   }
   else if (resultado >= 34.99) {
     divResultado.innerHTML = `IMC ${resultado} - ${tipoPeso[4]}`;
     divResultado.style.color = "#FFA500";
+    divResultado.classList.add('exibir');
+
   }
   else if (resultado >= 29.99) {
     divResultado.innerHTML = `IMC ${resultado} - ${tipoPeso[3]}`;
     divResultado.style.color = "#FFA500";
+    divResultado.classList.add('exibir');
+
   }
   else if (resultado >= 24.99) {
     divResultado.innerHTML = `IMC ${resultado} - ${tipoPeso[2]}`;
     divResultado.style.color = "#FFA500";
+    divResultado.classList.add('exibir');
+
   }
   else if (resultado >= 18.5) {
     divResultado.innerHTML = `IMC ${resultado} - ${tipoPeso[1]}`;
     divResultado.style.color = "#00FF00";
+    divResultado.classList.add('exibir');
   }
   else {
     divResultado.innerHTML = 'Favor conferir as informaçoes fornecidas!';
     divResultado.style.color = "red";
+    divResultado.classList.add('exibir');
+
   }
-
   console.log(resultado);
-
 }
 
 
